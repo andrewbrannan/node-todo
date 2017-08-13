278218a8c9092ae5298f674fbad4898f5db4f8ad
andrewbrannan <andrew@converge.io>
2017-08-13 19:12:26 +0100

    start db index at 1

--- todo_db.sql
+++ todo_db.sql
@@ -15,14 +15,14 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner:
 --
 
 CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner:
 --
 
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
@@ -87,7 +87,7 @@ COPY list (id, task, done) FROM stdin;
 -- Name: list_id_seq; Type: SEQUENCE SET; Schema: public; Owner: andrewbrannan
 --
 
-SELECT pg_catalog.setval('list_id_seq', 2, true);
+SELECT pg_catalog.setval('list_id_seq', 0, true);
 
 
 --
@@ -101,4 +101,3 @@ ALTER TABLE ONLY list
 --
 -- PostgreSQL database dump complete
 --
-

a9f32fcf0fd02785c992bc5063648b52d6e1df92
andrewbrannan <andrew@converge.io>
2017-08-13 17:58:04 +0100

    add structure for arg parsing

--- app.js
+++ app.js
@@ -1,6 +1,39 @@
 "use strict";
 
+var argv = require('minimist')(process.argv.slice(2));
 
-foo = "test"
 
-console.log(foo)
+
+if(argv._.length == 0){
+  printHelp();
+  process.exit(0);
+}
+
+switch(argv._[0]){
+    case "add":
+      break;
+    case "list":
+      break;
+    case "delete":
+      break;
+    case "complete":
+      break;
+    case "":
+      console.log("NOTHING");
+      break;
+
+    default:
+      console.log("Invalid Argument.")
+      printHelp();
+      process.exit(0);
+
+}
+
+function printHelp(){
+  console.log("~~~~~~~~~~TODO HELP~~~~~~~~~~~");
+  console.log("");
+  console.log("add <task>:     Add a task to the list");
+  console.log("list:           List all tasks");
+  console.log("complete <int>: Mark given task as complete");
+  console.log("delete <int>:   Delete given task from list");
+}

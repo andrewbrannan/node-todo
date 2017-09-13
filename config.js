99e5cab0bc8932c11b2d32386814006d24a27dec
andrewbrannan <andrew@converge.io>
2017-09-13 21:17:16 +0100

    add config file

---
+++ config.js
@@ -0,0 +1,9 @@
+"use strict"
+
+// Obviously in production this shouldn't be in source countrol ;)
+
+var config = {};
+
+config.jwtsecret = 'thequickbrownfoxjumpedoverthelazydog'
+
+module.exports=config
